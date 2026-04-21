bc188038bb1600dcd3bf8e7e0ffdbd3ecedd83c3
yespure <Ltc061115@gmail.com>
2026-04-21 15:34:01 +0800

    MainMenu?

---
+++ GDIM61 Project/Assets/Script/InGameMenu/FirstScene.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class FirstScene : MonoBehaviour
+{
+    private void Update()
+    {
+        bool keyInput = Input.anyKeyDown;
+        bool mouseInput = Input.GetMouseButtonDown(0) || Input.GetMouseButtonDown(1);
+        if (keyInput || mouseInput)
+        {
+            SceneManager.LoadScene("SampleScene");
+        }
+    }
+}

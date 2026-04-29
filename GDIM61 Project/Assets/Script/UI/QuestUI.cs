5483dd6be7e7b211c8abc295ac034986b8120434
random0624 <77592514+random0624@users.noreply.github.com>
2026-04-28 21:58:28 -0700

    Added QuestUI code

--- GDIM61 Project/Assets/Script/UI/QuestUI.cs
+++ GDIM61 Project/Assets/Script/UI/QuestUI.cs
@@ -1,6 +1,7 @@
 using System.Collections.Generic;
 using System;
 using UnityEngine;
+using TMPro;
 
 public class QuestUI : MonoBehaviour{
     public Transform questListContent;
@@ -8,13 +9,33 @@ public class QuestUI : MonoBehaviour{
     public GameObject objectiveTextPrefab;
     public Quest testQuest;
     public int testQuestAmount;
-    private List<QuestProgress> questProgressList;
+    private List<QuestProgress> testQuests = new();
     //Start is called before the first frame update
 
     public void Start(){
-
+        for(int i = 0; i < testQuestAmount; i++){
+            testQuests.Add(new QuestProgress(testQuest));
+        }
     }
-    public void Update(){
+    public void UpdateQuestUI(){
+        //Destroy existing quest entries
+        foreach(Transform child in questListContent){
+            Destroy(child.gameObject);
+        }
+        //Create new quest entries
+        foreach(var quest in testQuests){
+            GameObject entry = Instantiate(questEntryPrefab, questListContent);
+            TMP_Text questNameText = entry.transform.Find("QuestNameText").GetComponent<TMP_Text>();
+            Transform objectiveList = entry.transform.Find("ObjectiveList");
 
+            questNameText.text = quest.quest.name;
+
+            foreach(var objective in quest.objectives){
+                GameObject objTEXTGO = Instantiate(objectiveTextPrefab, objectiveList);
+                TMP_Text objText = objTEXTGO.GetComponent<TMP_Text>();
+                objText.text = $"{objective.description} ({objective.currentAmount}/{objective.requiredAmount})"; //Collect Treasure chest amt...
+            }
+        }
     }
 }
+
